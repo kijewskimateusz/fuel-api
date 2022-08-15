b32a9faf2eded78c9d46b4c178d19a04f8ec3a19
Mateusz Kijewski <mateusz@MacBook-Pro-suzbowy.local>
2022-08-15 13:25:45 +0200

    Added nested Image model

--- fuel-api/models.py
+++ fuel-api/models.py
@@ -1,21 +1,27 @@
-from typing import List
 from typing import Union
 
 from pydantic import BaseModel
+from pydantic import HttpUrl
 
 
 class Petroleum(BaseModel):
     id: int
     name: str
-    short_name: int
+    short_name: str
     description: Union[str, None] = None
     tax: Union[float, None] = None
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
 class PetrolStation(BaseModel):
     id: int
     name: str
-    sold_petroleum: List
+    sold_petroleum: set[str] = set()
     City: Union[str, None] = None
     Address: Union[str, None] = None
+    image: Union[Image, None] = None
     Active: bool
